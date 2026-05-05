48fc0601adc39561627c406e24d416b4a06d4469
pranto <pranto@bitmorpher.com>
2026-05-05 17:14:05 +0600

    travis.yml file added

---
+++ storekit-2-demo-app(Final)/storekit-2-demo-appTests/storekit_2_demo_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  storekit_2_demo_appTests.swift
+//  storekit-2-demo-appTests
+//
+//  Created by Photolab AI2 on 5/5/26.
+//
+
+import Testing
+
+struct storekit_2_demo_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
